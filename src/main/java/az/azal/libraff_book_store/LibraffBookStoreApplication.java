c9a951f41bc6b268b858d78f44aad3fa2451c56d
Mammadzada Onur Mahammad <onurmamedov1@gmail.com>
2026-02-23 11:12:03 +0400

    Project created

---
+++ src/main/java/az/azal/libraff_book_store/LibraffBookStoreApplication.java
@@ -0,0 +1,13 @@
+package az.azal.libraff_book_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraffBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraffBookStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/azal/libraff_book_store/LibraffBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package az.azal.libraff_book_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraffBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
